239edcd0e71f470f455f27972f0de162436aeedb
Brandon Cox <brandon.cox@slalom.com>
2022-04-18 19:43:30 -0600

    add form validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,35 +8,48 @@ import {
   Col,
   Button,
   Loading,
+  useInput,
+  FormElement,
 } from '@nextui-org/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import LoadsGrid from '../components/LoansGrid';
+import { phone as phoneValdation } from 'phone';
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Home: NextPage = () => {
   const [values, setValues] = useState({
     wallet: '',
-    phone: '',
-    ratio: '',
   });
+  const {
+    value: wallet,
+    setValue: setWallet,
+    bindings: walletBindings,
+  } = useInput('');
+  const {
+    value: ratio,
+    setValue: setRatio,
+    bindings: ratioBinding,
+  } = useInput('');
+  const {
+    value: phone,
+    setValue: setPhone,
+    bindings: phoneBindings,
+  } = useInput('');
   const [loans, setLoans] = useState(undefined);
+  const [santizedPhone, setSantizedPhone] = useState<string | undefined>(
+    undefined
+  );
+  const [phoneHelperText, setPhoneHelperText] = useState<string | undefined>(
+    undefined
+  );
+  const [ratioHelperText, setRatioHelperText] = useState<string | undefined>(
+    undefined
+  );
   const [loansLoading, setLoansLoading] = useState(false);
   const [submittingNotification, setSubmittingNotification] = useState(false);
-
-  const handleChange = (
-    key: 'wallet' | 'phone' | 'ratio',
-    value: string | number
-  ) => {
-    setValues((prev) => {
-      return {
-        ...prev,
-        [key]: value,
-      };
-    });
-  };
 
   const handleSubmit = async () => {
     setSubmittingNotification(true);
@@ -53,12 +66,13 @@ const Home: NextPage = () => {
     //   })
     // });
     await delay(1000);
+    console.log(santizedPhone, ratio);
     setSubmittingNotification(false);
   };
 
   const handleGetLoans = async () => {
     setLoansLoading(true);
-    const res = await fetch(`api/loans/${values.wallet}`, {
+    const res = await fetch(`api/loans/${walletBindings.value}`, {
       method: 'GET',
       headers: {
         'Cache-Control': 'max-age=604800',
@@ -69,6 +83,33 @@ const Home: NextPage = () => {
     setLoansLoading(false);
   };
 
+  const handlePhoneChange = (event: ChangeEvent<FormElement>) => {
+    const { isValid, phoneNumber } = phoneValdation(event.target.value);
+    if (!isValid) {
+      setPhoneHelperText('Phone number is invalid.');
+      setSantizedPhone(undefined);
+    } else {
+      setPhoneHelperText(undefined);
+      setSantizedPhone(phoneNumber);
+    }
+    phoneBindings.onChange(event);
+  };
+
+  const handleRatioChange = (event: ChangeEvent<FormElement>) => {
+    const ratio = event.target.value;
+    if (Number(ratio) < 1.01 || Number(ratio) > 1.3) {
+      setRatioHelperText('Ratio must be between 1.0 and 1.3');
+    } else {
+      setRatioHelperText(undefined);
+    }
+    ratioBinding.onChange(event);
+  };
+
+  const submitDisabled = (): boolean => {
+    if (!ratioHelperText && santizedPhone && santizedPhone.length) return false;
+    return true;
+  };
+
   return (
     <div style={{ padding: '0 2rem' }}>
       <Head>
@@ -76,7 +117,7 @@ const Home: NextPage = () => {
         <meta name='description' content='Folks Notifier' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Spacer y={3} />
+      <Spacer y={2} />
       <main>
         <Container sm css={{ minHeight: '80vh' }} justify='center'>
           <Row gap={1} align='center' justify='center'>
@@ -95,12 +136,12 @@ const Home: NextPage = () => {
           <Row gap={1} align='flex-end'>
             <Col span={9}>
               <Input
-                value={values.wallet}
+                value={walletBindings.value}
                 fullWidth
                 size='md'
                 label='Wallet Address'
                 placeholder='7BZEUIEPHZGDK6E673DVOY6BVCCZC6YFAJ3QWROPBZK5XKGE5GUWDYZRUY'
-                onChange={(e) => handleChange('wallet', e.target.value)}
+                onChange={walletBindings.onChange}
               />
             </Col>
             <Col span={2}>
@@ -108,7 +149,7 @@ const Home: NextPage = () => {
                 color='gradient'
                 size='md'
                 onClick={handleGetLoans}
-                disabled={values.wallet.length !== 58}
+                disabled={walletBindings.value.length !== 58}
               >
                 {loansLoading ? (
                   <Loading
@@ -130,29 +171,35 @@ const Home: NextPage = () => {
               <Row gap={1}>
                 <Col span={8}>
                   <Input
-                    value={values.phone}
+                    value={phoneBindings.value}
                     fullWidth
                     size='md'
                     label='SMS Number'
                     placeholder='+1 (123)123-1234'
-                    onChange={(e) => handleChange('phone', e.target.value)}
+                    onChange={handlePhoneChange}
+                    helperText={phoneHelperText}
                   />
                 </Col>
                 <Col span={4}>
                   <Input
-                    value={values.ratio}
+                    value={ratioBinding.value}
                     fullWidth
                     size='md'
                     label='Collaral Ratio'
                     type='number'
-                    placeholder='1.1'
-                    onChange={(e) => handleChange('ratio', e.target.value)}
+                    onChange={handleRatioChange}
+                    helperText={ratioHelperText}
                   />
                 </Col>
               </Row>
               <Spacer y={2} />
               <Row gap={1} align='center' justify='center'>
-                <Button color='gradient' size='md' onClick={handleSubmit}>
+                <Button
+                  color='gradient'
+                  size='md'
+                  onClick={handleSubmit}
+                  disabled={submitDisabled()}
+                >
                   {submittingNotification ? (
                     <Loading
                       type='points-opacity'
